1afff8647a8f5758983d2f85e9b6359494956010
Rora Alem <rora.goitom@gmail.com>
2024-01-17 20:27:31 -0500

    Add dynamic CSS variable updates on input change

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,17 @@
+//select all input elements with class'.controls input'
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  //Get the dataset(data-sizing) and value from the input
+  const suffix = this.dataset.sizing || '';
+  const value = this.value;
+
+  //Update the CSS variable
+  document.documentElement.style.setProperty(`--${this.name}`, value + suffix)
+
+}
+
+// Listen for change on each input element
+inputs.forEach(input => {
+    input.addEventListener('change', handleUpdate) // call function when input value changes
+});
