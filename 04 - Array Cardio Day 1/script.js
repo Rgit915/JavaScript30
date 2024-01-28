1b030602afde8ea4d1e0bece455ee4bd7b5dd087
Rora Alem <rora.goitom@gmail.com>
2024-01-27 19:14:33 -0500

    Sort inventors by birthdate oldest to youngest

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -48,6 +48,12 @@ console.table(firstLastName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const oldestToYoungest = inventors.sort((a, b) =>{
+  a.year > b.year ? 1: -1 ;
+})
+
+console.log("Sort Oldest to Youngest inventors:");
+console.table(oldestToYoungest);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
