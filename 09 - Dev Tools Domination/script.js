215b7353587174c9190c29885666c0014791d37f
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-05 20:05:18 -0500

    Add some console tricks

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -7,3 +7,11 @@ const changeColor = () => {
   container.style.background = 'linear-gradient(to right, #ffcc00, #ff99cc)';
   container.style.color = 'white';
 }
+
+// Console Tricks
+console.log('Regular Text');
+console.log('Interpolated: My cat is %s, and she is %d years old.', cats[0].name, cats[0].age);
+console.log('%cStyled Text', 'background: linear-gradient(to right, #ffcc00, #ff99cc); color: transparent; font-size: 20px;');
+console.warn('This is a warning!');
+console.error('This is an error!');
+console.info('This is some information.');
