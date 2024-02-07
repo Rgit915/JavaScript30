18357429fc5a923e6a6ad948daa5b1a8c76dcd78
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-07 08:25:14 -0500

    Add updateButton function for play/pause button icon and eventlisteners for video play & pause

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -14,7 +14,16 @@ function togglePlay() {
   // Using the 'play' or 'pause' method based on the 'paused' property
   video[method]();
 }
+// Function to update the play/pause button icon based on video playback state
+function updateButton() {
+  // Ternary operator to determine the appropriate icon based on video pause status
+  const icon = this.paused ? '►' : '❚ ❚';
+  // Set the text content of the toggle button to the determined icon
+  toggle.textContent = icon;
+}
 
-// Event listeners to toggle play/pause on video click and play button click
-video.addEventListener('click', togglePlay);
+// Event listeners to toggle play/pause on video click and update button icon
+video.addEventListener('click', togglePlay);  
+video.addEventListener('play', updateButton); // Update button on video play
+video.addEventListener('pause', updateButton); // Update button on video pause
 toggle.addEventListener('click', togglePlay);
