257c57d4a6b17ed75765c6aa75214b97c567cf22
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-08 12:40:07 -0500

    Implement fullscreen mode functionality for custom video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = document.getElementById('fullscreenButton');
 
 // Function to toggle play/pause of the video
 function togglePlay() {
@@ -49,6 +50,17 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+// Function to toggle fullscreen mode
+function toggleFullscreen() {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  }
+}
+
 // Event listeners to toggle play/pause on video click and update button icon
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton); // Update button on video play
@@ -69,3 +81,5 @@ progress.addEventListener('click', scrub); // Scrub on click
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e)); // Scrub on mousemove if mousedown
 progress.addEventListener('mousedown', () => mousedown = true); // Set mousedown to true on mouse down
 progress.addEventListener('mouseup', () => mousedown = false); // Set mousedown to false on mouse up
+
+fullscreenButton.addEventListener('click', toggleFullscreen);
