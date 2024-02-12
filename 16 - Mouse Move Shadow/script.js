4811b1489242f20f273042a536fa63d9d31f20ef
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-12 18:25:06 -0500

    Set up dynamic text shadow based on mouse movement

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -17,9 +17,22 @@ function shadow(e){
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
+  
+// Setting up the text shadow based on mouse movement
+const walk = 300; // 100px
+const xWalk = Math.round((x / width * walk) - (walk / 2));
+const yWalk = Math.round((y / height * walk) - (walk / 2));
 
-  // Logging the X and Y coordinates to the console
-  console.log(x, y);
+// Applying the text shadow to the 'h1' element with dynamic values
+text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+                        ${xWalk * -1}px ${yWalk}px 0 rgba(167, 15, 255, 0.7),
+                        ${yWalk}px ${xWalk * -1}px 0 rgba(110, 255, 11, 0.7),
+                        ${yWalk * -1}px ${xWalk}px 0 rgba(0, 90, 255, 0.7)`;
+text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+${xWalk * -1}px ${yWalk}px 0 rgba(167,15,255,0.7),
+${yWalk}px ${xWalk * -1}px 0 rgba(110,255,11,0.7),
+${yWalk * -1}px ${xWalk}px 0 rgba(0,90,255,0.7)
+`;
 }
 
 // Adding a 'mousemove' event listener to the '.hero' element, which triggers the 'shadow' function
