4875df1a52c0cea9529e60d75790c916921269b8
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-16 11:37:45 -0500

    Implemented event listener for the 'result' event to process and display speech transcriptions

--- 20 - Speech Detection/script.js
+++ 20 - Speech Detection/script.js
@@ -13,11 +13,16 @@ let p = document.createElement('p');
 // Select the container for displaying recognized words
 const words = document.querySelector('.words');
 words.appendChild(p);
-
 // Event listener for the 'result' event triggered when speech is recognized
 recognition.addEventListener('result', e => {
-  
-  console.log(e);
+  // Extract and concatenate transcriptions from speech recognition results
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  // Display the transcribed speech in a paragraph element
+  p.textContent = transcript;
 });
 
 // Start the speech recognition
