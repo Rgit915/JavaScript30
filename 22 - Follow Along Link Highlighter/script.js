25ef9b692902621617a2345443adb8a7e3f47a04
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-18 13:38:42 -0500

    Calculate and store link coordinates, considering page scrolling

--- 22 - Follow Along Link Highlighter/script.js
+++ 22 - Follow Along Link Highlighter/script.js
@@ -11,11 +11,18 @@ function highlightLink() {
   // Get position and dimensions of the hovered link
   const linkCoords = this.getBoundingClientRect();
   console.log(linkCoords); // Log link coordinates for debugging
+  // Create an object to store the dimensions and position of the hovered link
+  const coords = {
+    width: linkCoords.width,            // Width of the link
+    height: linkCoords.height,          // Height of the link
+    top: linkCoords.top + window.scrollY, // Top position of the link, accounting for vertical scrolling
+    left: linkCoords.left + window.scrollX // Left position of the link, accounting for horizontal scrolling
+  };
 
   // Set the width, height, and position of the highlight span
-  highlight.style.width = `${linkCoords.width}px`;
-  highlight.style.height = `${linkCoords.height}px`;
-  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 // Add event listener to each link for highlighting on mouseenter
