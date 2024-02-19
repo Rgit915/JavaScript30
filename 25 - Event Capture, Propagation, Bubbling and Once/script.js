803140cb3ae5ba21251c32b1891f1a8203482754
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-19 17:53:46 -0500

    Add click event listeners with once option

--- 25 - Event Capture, Propagation, Bubbling and Once/script.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -1,10 +1,22 @@
 // Select all div elements on the page
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 
 // Function to log the classList value of a clicked div
 function logText(e) {
   console.log(this.classList.value);
+  e.stopPropagation();  // Stop event bubbling
 }
 
 // Add click event listener to each div, triggering logText function
-divs.forEach(div => div.addEventListener('click', logText ));
+divs.forEach(div => div.addEventListener('click', logText, {
+  capture: false,
+  once: true
+} ));
+
+// Add click event listener to the button, logging 'Click' once
+button.addEventListener('click', () =>{
+  console.log('Click');
+}, {
+  once: true
+})
