322bc6430bceea9163c4e222bfacae261875b39c
Rora Alem <91286482+Rgit915@users.noreply.github.com>
2024-02-20 18:21:22 -0500

    Initialize variables for slider functionality

---
+++ 27 - Click and Drag/script.js
@@ -0,0 +1,7 @@
+// Selecting the element with the class 'items' for the slider functionality
+const slider = document.querySelector('.items');
+
+// Variables to track slider state and mouse position
+let isDown = false;
+let startX;
+let scrollLeft;
